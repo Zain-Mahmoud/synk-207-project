79b1c1c0d9b6f0d9ec24fc4c8079b021684e382c
SleepyPandas <tommyrobotics1@gmail.com>
2025-11-19 23:09:40 -0500

    Implemented and Create CRUD methods except Update Task

--- src/main/java/data_access/TaskHabitDataAccessObject.java
+++ src/main/java/data_access/TaskHabitDataAccessObject.java
@@ -1,6 +1,7 @@
 package data_access;
 
 import entities.Task;
+import jdk.jshell.spi.ExecutionControl;
 import use_case.gateways.TaskGateway;
 
 import java.util.ArrayList;
@@ -15,7 +16,7 @@ public class TaskHabitDataAccessObject implements TaskGateway {
 	private final Map<String, ArrayList<Task>> userTasks = new HashMap<>();
 
 	@Override
-	public String addTask(String userId, Task task) {
+    public String addTask(String userId, Task task) {
         // Compute if absent to initialize user's task list, if it exists return it
 
 		ArrayList<Task> tasksForUser = userTasks.computeIfAbsent(userId, key -> new ArrayList<>());
@@ -30,7 +31,7 @@ public class TaskHabitDataAccessObject implements TaskGateway {
 	}
 
 	@Override
-	public ArrayList<Task> fetchTasks(String userId) {
+    public ArrayList<Task> fetchTasks(String userId) {
 		ArrayList<Task> tasks = userTasks.get(userId);
 
         if (tasks == null) {
@@ -43,7 +44,7 @@ public class TaskHabitDataAccessObject implements TaskGateway {
 
 
 	@Override
-	public boolean deleteTask(String userId, Task task) {
+    public boolean deleteTask(String userId, Task task) {
 		ArrayList<Task> tasks = userTasks.get(userId);
 		if (tasks == null) {
 			return false;
@@ -55,4 +56,22 @@ public class TaskHabitDataAccessObject implements TaskGateway {
         }
 		return false;
 	}
+
+    /**
+     * @param userId
+     * @param task
+     * @return
+     */
+
+    // Just testing Not implemented exception, I remember there was an easy way to do it in Java,
+    // but I forgor
+    @Override
+    public boolean updateTask(String userId, Task task) {
+        try {
+            throw new ExecutionControl.NotImplementedException("TODO");
+        } catch (ExecutionControl.NotImplementedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
