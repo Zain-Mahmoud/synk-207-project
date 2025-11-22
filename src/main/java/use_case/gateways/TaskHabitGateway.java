d542daf099586a69dfbf4bc869b2d3797a3d1225
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2025-11-22 09:01:17 -0500

    modified task habit object

--- src/main/java/use_case/gateways/TaskHabitGateway.java
+++ src/main/java/use_case/gateways/TaskHabitGateway.java
@@ -2,17 +2,23 @@ package use_case.gateways;
 
 // CRUD GET (read), PUT (create and update), POST (create - if we don't have `id` or `uuid`), and DELETE (delete)
 
+import entities.Habit;
 import entities.Task;
 
 import java.util.ArrayList;
 
-public interface TaskGateway {
+public interface TaskHabitGateway {
 
     String addTask(String userId, Task task);
 
     ArrayList<Task> fetchTasks(String userId);
 
     boolean deleteTask(String userId, Task task);
+
+    boolean deleteHabit(String userId, Habit habit);
+
+    String addHabit(String userId, Habit habit);
+
 
 
 }
