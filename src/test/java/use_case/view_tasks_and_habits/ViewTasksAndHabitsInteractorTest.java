e08f44ef78fa2ae2491358e0a7da95d4df2c68ec
AryaSaravanan <118869865+AryaSaravanan@users.noreply.github.com>
2025-12-02 00:27:39 -0500

    finished the implementation of the test

--- src/test/java/use_case/view_tasks_and_habits/ViewTasksAndHabitsInteractorTest.java
+++ src/test/java/use_case/view_tasks_and_habits/ViewTasksAndHabitsInteractorTest.java
@@ -5,24 +5,23 @@ import data_access.InMemoryTaskDataAccessObject;
 import data_access.InMemoryUserDataAccessObject;
 import entities.*;
 import interface_adapter.ViewManagerModel;
-import interface_adapter.logged_in.LoggedInState;
-import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.view_tasks_and_habits.ViewTasksAndHabitsPresenter;
 import interface_adapter.view_tasks_and_habits.ViewTasksAndHabitsState;
 import interface_adapter.view_tasks_and_habits.ViewTasksAndHabitsViewModel;
+import interface_adapter.logged_in.LoggedInViewModel;
+import org.junit.jupiter.api.Test;
 import use_case.gateways.HabitGateway;
 import use_case.gateways.TaskGateway;
-import interface_adapter.view_tasks_and_habits.ViewTasksAndHabitsPresenter;
 import use_case.gateways.UserGateway;
-import use_case.view_stats.ViewStatsOutputData;
-
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ViewTasksAndHabitsInteractorTest {
 
+    @Test
     void SuccessTest() {
 
         TaskGateway taskGateway = new InMemoryTaskDataAccessObject();
@@ -30,21 +29,20 @@ public class ViewTasksAndHabitsInteractorTest {
         UserGateway userGateway = new InMemoryUserDataAccessObject();
 
         LoggedInViewModel loggedInViewModel = new LoggedInViewModel();
-        ViewTasksAndHabitsViewModel correctViewModel = new  ViewTasksAndHabitsViewModel();
-        ViewTasksAndHabitsViewModel testViewModel = new  ViewTasksAndHabitsViewModel();
-        ViewManagerModel viewManagerModel = new  ViewManagerModel();
-        ViewTasksAndHabitsPresenter correctPresenter = new ViewTasksAndHabitsPresenter(viewManagerModel, correctViewModel);
-        ViewTasksAndHabitsPresenter testPresenter = new ViewTasksAndHabitsPresenter(viewManagerModel, testViewModel);
+
+        ViewTasksAndHabitsViewModel testViewModel = new ViewTasksAndHabitsViewModel();
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        TestPresenter presenter = new TestPresenter(viewManagerModel, testViewModel);
 
         TaskBuilder taskBuilder = new TaskBuilder();
         HabitBuilder habitBuilder = new HabitBuilder();
 
-        Task task = taskBuilder
+        Task task1 = taskBuilder
                 .setTaskGroup("Problem set")
-                .setTaskName("Problem set")
-                .setStartTime(LocalDateTime.parse( "2025-11-26T00:00:00"))
-                .setDeadline(LocalDateTime.parse( "2025-11-27T00:00:00"))
-                .setStatus(true)
+                .setTaskName("Problem Set")
+                .setStartTime(LocalDateTime.parse("2025-11-26T00:00:00"))
+                .setDeadline(LocalDateTime.parse("2025-11-27T00:00:00"))
+                .setStatus(false)
                 .setPriority(2)
                 .setDescription("Finishing a problem set")
                 .build();
@@ -52,92 +50,119 @@ public class ViewTasksAndHabitsInteractorTest {
         Task task2 = taskBuilder
                 .setTaskGroup("project")
                 .setTaskName("project")
-                .setStartTime(LocalDateTime.parse( "2025-11-26T00:00:00"))
-                .setDeadline(LocalDateTime.parse( "2025-11-27T00:00:00"))
+                .setStartTime(LocalDateTime.parse("2025-11-26T00:00:00"))
+                .setDeadline(LocalDateTime.parse("2025-11-27T00:00:00"))
                 .setStatus(false)
                 .setPriority(2)
                 .setDescription("Finishing a project")
                 .build();
 
-        Habit habit = habitBuilder.setHabitName("Not programming")
+        Habit habit1 = habitBuilder.setHabitName("Not programming")
                 .setHabitGroup("Programming")
                 .setFrequency(1)
-                .setStartDateTime(LocalDateTime.parse( "2025-11-27T00:00:00"))
+                .setStartDateTime(LocalDateTime.parse("2025-11-27T00:00:00"))
                 .setPriority(1)
                 .setStatus(true)
                 .setStreakCount(1)
                 .setDescription("Not doing some programming")
                 .build();
+
         Habit habit2 = habitBuilder.setHabitName("Programming")
                 .setHabitGroup("Programming")
                 .setFrequency(1)
-                .setStartDateTime(LocalDateTime.parse( "2025-11-27T00:00:00"))
+                .setStartDateTime(LocalDateTime.parse("2025-11-27T00:00:00"))
                 .setPriority(4)
                 .setStatus(false)
                 .setStreakCount(10)
                 .setDescription("Doing some programming")
                 .build();
 
-        UserFactory userFactory = new UserFactory();
-
-        User arya = userFactory.create("arya", "123");
+        UserFactory factory = new UserFactory();
+        User arya = factory.create("arya", "123");
         userGateway.save(arya);
 
-        taskGateway.addTask("arya", task);
+        taskGateway.addTask("arya", task1);
         taskGateway.addTask("arya", task2);
-        habitGateway.addHabit("arya", habit);
+        habitGateway.addHabit("arya", habit1);
         habitGateway.addHabit("arya", habit2);
 
-        ViewTasksAndHabitsInteractor viewTasksAndHabitsInteractor = new ViewTasksAndHabitsInteractor(taskGateway,
-                habitGateway, testPresenter);
+        ViewTasksAndHabitsInteractor interactor =
+                new ViewTasksAndHabitsInteractor(taskGateway, habitGateway, presenter);
 
-        ArrayList<String> formattedTask = new ArrayList<>();
-        formattedTask.add("Problem Set");
-        formattedTask.add("26 November 25, 00:00");
-        formattedTask.add("27 November 25, 00:00");
-        formattedTask.add("Problem Set");
-        formattedTask.add("Incomplete");
-        formattedTask.add("2");
-        formattedTask.add("Finishing a problem set");
+        ArrayList<String> f1 = new ArrayList<>();
+        f1.add("Problem Set");
+        f1.add("26 November 25, 00:00");
+        f1.add("27 November 25, 00:00");
+        f1.add("Problem Set");
+        f1.add("Incomplete");
+        f1.add("2");
+        f1.add("Finishing a problem set");
 
-        ArrayList<String> formattedTask2 = new ArrayList<>();
-        formattedTask.add("project");
-        formattedTask.add("26 November 25, 00:00");
-        formattedTask.add("27 November 25, 00:00");
-        formattedTask.add("project");
-        formattedTask.add("Incomplete");
-        formattedTask.add("2");
-        formattedTask.add("Finishing a project");
+        ArrayList<String> f2 = new ArrayList<>();
+        f2.add("project");
+        f2.add("26 November 25, 00:00");
+        f2.add("27 November 25, 00:00");
+        f2.add("project");
+        f2.add("Incomplete");
+        f2.add("2");
+        f2.add("Finishing a project");
 
-        ArrayList<String> formattedHabit = new ArrayList<>();
-        formattedHabit.add("Not programming");
-        formattedHabit.add("27 November 25, 00:00");
-        formattedHabit.add("1");
-        formattedHabit.add("Programming");
-        formattedHabit.add("1");
-        formattedHabit.add("Not doing some programming");
+        ArrayList<String> h1 = new ArrayList<>();
+        h1.add("Not programming");
+        h1.add("27 November 25, 00:00");
+        h1.add("1");
+        h1.add("Programming");
+        h1.add("1");
+        h1.add("Not doing some programming");
 
-        ArrayList<String> formattedHabit2 = new ArrayList<>();
-        formattedTask.add("Programming");
-        formattedTask.add("27 November 25, 00:00");
-        formattedTask.add("1");
-        formattedTask.add("Programming");
-        formattedTask.add("1");
-        formattedTask.add("Doing some  programming");
+        ArrayList<String> h2 = new ArrayList<>();
+        h2.add("Programming");
+        h2.add("27 November 25, 00:00");
+        h2.add("1");
+        h2.add("Programming");
+        h2.add("1");
+        h2.add("Doing some programming");
 
-        ArrayList<ArrayList<String>> formattedTasks = new ArrayList<>();
-        formattedTasks.add(formattedTask);
-        formattedTasks.add(formattedTask2);
+        interactor.getFormattedTasksAndHabits(loggedInViewModel);
 
-        ArrayList<ArrayList<String>> formattedHabits = new ArrayList<>();
-        formattedHabits.add(formattedHabit);
-        formattedHabits.add(formattedHabit2);
+        ViewTasksAndHabitsState resultState = testViewModel.getState();
+        assertNotNull(resultState);
 
-        ViewTasksAndHabitsOutputData correctOutputData = new ViewTasksAndHabitsOutputData(formattedTasks, formattedHabits);
-        correctPresenter.prepareSuccessView(correctOutputData);
+        assertEquals(2, resultState.getFormattedTasks().size());
+        assertEquals(2, resultState.getFormattedHabits().size());
 
-        viewTasksAndHabitsInteractor.getFormattedTasksAndHabits(loggedInViewModel);
+        assertEquals(f1, resultState.getFormattedTasks().get(0));
+        assertEquals(f2, resultState.getFormattedTasks().get(1));
 
+        assertEquals(h1, resultState.getFormattedHabits().get(0));
+        assertEquals(h2, resultState.getFormattedHabits().get(1));
+    }
 
+    // --------------------------------
+    // Minimal presenter for capturing output
+    // --------------------------------
+    private static class TestPresenter extends ViewTasksAndHabitsPresenter {
+
+        private final ViewTasksAndHabitsViewModel vm;
+
+        TestPresenter(ViewManagerModel viewManagerModel, ViewTasksAndHabitsViewModel vm) {
+            super(viewManagerModel, vm);
+            this.vm = vm;
+        }
+
+        @Override
+        public void prepareSuccessView(ViewTasksAndHabitsOutputData data) {
+            ViewTasksAndHabitsState state = new ViewTasksAndHabitsState();
+            state.setFormattedTasks(data.getFormattedTasks());
+            state.setFormattedHabits(data.getFormattedHabits());
+            vm.setState(state);
+        }
+
+        @Override
+        public void prepareFailView(String error) {
+            fail("Interactor should not fail in this test");
+        }
     }
 }
+
+
